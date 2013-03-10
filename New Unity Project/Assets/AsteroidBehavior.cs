4d795f3cb7f9c68bd3624b356f166aabd3ef3e2e
John <jodavidg@cisco.com>
2013-03-10 14:44:07 -0700

    Added deorbited asteroid counter.

--- New Unity Project/Assets/AsteroidBehavior.cs
+++ New Unity Project/Assets/AsteroidBehavior.cs
@@ -56,6 +56,7 @@ public class AsteroidBehavior : MonoBehaviour {
 		if (collision.gameObject == GameObject.Find("Planet")){
 			GameObject.Find("Main Camera").audio.Play();
 			MoneyCounterScript.money = MoneyCounterScript.money + (int)((transform.localScale.x *1000)/150);
+			MoneyCounterScript.deorbit_count++;
         	Destroy(gameObject);
 		}
 	}
