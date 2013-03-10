3995f32e3a9a8eb3c52e807c40639dfb1677f02f
HungryHomeless <crezers@live.co.uk>
2013-03-10 16:57:20 -0700

    d

--- New Unity Project/Assets/MenuScript.cs
+++ New Unity Project/Assets/MenuScript.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class MenuScript : MonoBehaviour {
 
 	void OnGUI () {
-        GUI.TextArea(new Rect((Screen.width - 220),(Screen.height - 680),80,20),"CONTROLS");
-        GUI.TextArea(new Rect((Screen.width - 260),(Screen.height - 650),160,230),
+        GUI.TextArea(new Rect((Screen.width - 220),(Screen.height - 700),80,20),"CONTROLS");
+        GUI.TextArea(new Rect((Screen.width - 260),(Screen.height - 670),160,230),
 			"Up: Increase Thrust" + System.Environment.NewLine +
 			System.Environment.NewLine +
 			"Down: Decrease Thrust" + System.Environment.NewLine +
@@ -21,23 +21,14 @@ public class MenuScript : MonoBehaviour {
 			"G: Gyroscope On/Off" + System.Environment.NewLine +
 			System.Environment.NewLine +
 			"Spacebar: Grabber On/Off" + System.Environment.NewLine);
-		
-		
-		// Make a group on the center of the screen
-		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 , 100, 100));
-		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
 
-		// We'll make a box so you can see where the group is on-screen.
-		GUI.Box (new Rect (0,0,100,100), "Menu");
-		if (GUI.Button (new Rect (10,25,80,30), "Start Game")){
+	// We'll make a box so you can see where the group is on-screen.
+		GUI.Box (new Rect ((Screen.width / 2),(Screen.height - 700),100,100), "Menu");
+		if (GUI.Button (new Rect (((Screen.width / 2) + 10),(Screen.height - 675),80,30), "Start Game")){
 			Application.LoadLevel("mainScene");		
 		}
-		if (GUI.Button (new Rect (10,65,80,30), "Quit")){
+		if (GUI.Button (new Rect (((Screen.width / 2) + 10),(Screen.height - 635),80,30), "Quit")){
 			Application.Quit();
 		}
-
-		// End the group we started above. This is very important to remember!
-		GUI.EndGroup ();
 	}
-
 }
