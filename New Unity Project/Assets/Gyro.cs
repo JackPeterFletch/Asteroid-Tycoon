cc0435240fd7fcec6f5ff01a950afdbfcff0b617
HungryHomeless <crezers@live.co.uk>
2013-03-09 22:18:28 -0800

    d

---
+++ New Unity Project/Assets/Gyro.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Gyro : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//var spaceShuttle = GameObject.Find("SpaceShuttleOrbiter");
+		//if (spaceShuttle.gyro == true){
+		//	this.guiText.text = "Gyro: On";
+		//} else {
+		//	this.guiText.text = "Gyro: Off";
+		//}
+	}
+}
+
