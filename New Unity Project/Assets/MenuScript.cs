2a18e55ace8e22b6a66dcd7200501c419890167f
HungryHomeless <crezers@live.co.uk>
2013-03-10 15:53:06 -0700

    d

--- New Unity Project/Assets/MenuScript.cs
+++ New Unity Project/Assets/MenuScript.cs
@@ -4,6 +4,25 @@ using System.Collections;
 public class MenuScript : MonoBehaviour {
 
 	void OnGUI () {
+        GUI.TextArea(new Rect((Screen.width - 220),(Screen.height - 650),80,20),"CONTROLS");
+        GUI.TextArea(new Rect((Screen.width - 260),(Screen.height - 620),160,230),
+			"Up: Increase Thrust" + System.Environment.NewLine +
+			System.Environment.NewLine +
+			"Down: Decrease Thrust" + System.Environment.NewLine +
+			System.Environment.NewLine +
+			"W: Pitch Down" + System.Environment.NewLine +
+			System.Environment.NewLine +
+			"S: Pitch Up" + System.Environment.NewLine +
+			System.Environment.NewLine +
+			"X: Zoom Out" + System.Environment.NewLine +
+			System.Environment.NewLine +
+        	"Z: Zoom In" + System.Environment.NewLine +
+			System.Environment.NewLine +
+			"G: Gyroscope On/Off" + System.Environment.NewLine +
+			System.Environment.NewLine +
+			"Spacebar: Grabber On/Off" + System.Environment.NewLine);
+		
+		
 		// Make a group on the center of the screen
 		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 , 100, 100));
 		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
