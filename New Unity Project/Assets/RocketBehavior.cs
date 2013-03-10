3841de4412782ffce671e97479e2626ef0b405b6
John <jodavidg@cisco.com>
2013-03-09 22:47:48 -0800

    Stuff

--- New Unity Project/Assets/RocketBehavior.cs
+++ New Unity Project/Assets/RocketBehavior.cs
@@ -17,7 +17,7 @@ public class RocketBehavior : MonoBehaviour {
 		this.rigidbody.velocity = orbital_velocity;
 		positions = new System.Collections.Generic.List<Vector3>();
 		phantom = GameObject.Find("Phantom Shuttle");
-		gyro = true;
+		gyro = false;
 		phantom.transform.position = this.transform.position;
       	phantom.rigidbody.velocity = this.rigidbody.velocity * 10;
 		
